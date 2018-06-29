a9aa2b876ecbe0d854d416252844f83f467e6f1e
Antony74 <akb@akb.me.uk>
2018-06-29 10:52:51 +0100

    Add missing webpack-dev-server dependency. Add missing package-lock.json file. Add a "Hello, World!" sketch to make it obvious when the system is working.

--- src/js/setup.js
+++ src/js/setup.js
@@ -3,5 +3,9 @@ window.setup = function () {
   console.log("Setup")
 }
 window.draw = function () {
-  console.log("Draw")
+  window.background(230);
+  window.fill(255, 0, 255);
+  window.ellipse(50, 50, 70, 70);
+  window.noloop();
 }
+
