146080191c54739512617cc00d81ad2ab38ad9d6
Antony <akb@akb.me.uk>
2018-06-29 22:31:57 +0100

    Upgrade webpack & friends.  Accidentally lose sass while hacking around with webpack.config.js... but since it wasn't being used anyway...

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,4 @@
 var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
-
-var prod = process.env.NODE_ENV === 'prod'
-var dev = process.env.NODE_ENV === 'dev'
 
 module.exports = {
 	entry: './src/index.js',
@@ -12,8 +7,11 @@ module.exports = {
 		filename: 'bundle.js'
 	},
 	resolve: {
-		root: path.resolve(__dirname),
-		extensions: ['', '.js', '.scss'],
+		modules: [
+			path.resolve(__dirname),
+			'node_modules'
+		],
+		extensions: ['.js', '.scss'],
 		alias: {
 			src: path.resolve(__dirname) + '/src',
 			scss: path.resolve(__dirname) + '/src/scss',
@@ -21,17 +19,24 @@ module.exports = {
 		},
 	},
 	module: {
-		loaders: [
+		rules: [
 			{ 
 				test: /\.js$/, 
 				exclude: /node_modules/, 
-				loader: 'babel-loader' 
+				loader: [
+					'babel-loader'
+				] 
+			},
+			{
+				test: /\.scss$/,
+				loaders: [
+					'style-loader',
+					'css-loader']
 			}
 		]
 	},
 	devServer: {
 		progress: true,
-		colors: true,
 		inline: true,
 		historyApiFallback: {
 			index: '/test/'
@@ -40,36 +45,3 @@ module.exports = {
 	plugins: []
 };
 
-/*
- * Dev
- * --------------------------------------------------
- */
-if (dev) {
-	module.exports.module.loaders = (module.exports.module.loaders || []).concat([
-		{
-			test: /\.scss$/,
-			loaders: ['style', 'css', 'sass']
-		},
-	])
-}
-
-/*
- * Prod
- * --------------------------------------------------
- */
-if (prod) {
-	module.exports.module.loaders = (module.exports.module.loaders || []).concat([
-		{ 
-			test: /\.scss$/, 
-			loader: StyleExtHtmlWebpackPlugin.inline('sass')
-		}
-	])
-	module.exports.plugins = (module.exports.plugins || []).concat([
-		new HtmlWebpackPlugin({
-			cache: false,
-			title: 'p5js Experiment',
-			template: './src/templates/index.ejs'
-		}),
-		new StyleExtHtmlWebpackPlugin()
-	])
-}
