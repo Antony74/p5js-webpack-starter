482324f4485fefc77249eb589ea838250ca39ef9
Antony <akb@akb.me.uk>
2018-06-30 22:10:04 +0100

    Switch from global to instance mode.

--- src/js/setup.js
+++ src/js/setup.js
@@ -1,11 +1,19 @@
-import 'p5'
-window.setup = function () {
-  console.log("Setup")
-}
-window.draw = function () {
-  window.background(230);
-  window.fill(255, 0, 255);
-  window.ellipse(50, 50, 70, 70);
-  window.noLoop();
-}
+const P5 = require('p5');
+
+new P5((p5) => {
+ 
+    var x = 100; 
+    var y = 100;
+
+    p5.setup = () => {
+      console.log('setup');
+    };
+
+    p5.draw = () => {
+      p5.background(230);
+      p5.fill(255, 0, 255);
+      p5.ellipse(50, 50, 70, 70);
+      p5.noLoop();
+    };
+});
 
